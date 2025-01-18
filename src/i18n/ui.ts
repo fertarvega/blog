511f4a5a2e9700efa4dd12c37cebb724f7d0fd3c
Fernando Tarango <fertarvega@gmail.com>
2025-01-18 14:56:17 -0600

    Add multiple languages, images need fix and default language / page needs fix

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+  es: "Es",
+  en: "En",
+};
+
+export const defaultLang = "es";
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+  },
+  es: {
+    "nav.home": "Inicio",
+    "nav.about": "Sobre mí",
+  },
+} as const;
+
+export const showDefaultLang = false;
